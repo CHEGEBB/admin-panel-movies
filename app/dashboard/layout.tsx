3f9f41c5ad40d3e589d7a3e925f806c2158a64ae
CHEGEBB <chegephil24@gmail.com>
2026-05-02 14:48:09 +0300

    feat: add 'All Series' navigation option and corresponding icon in DashboardLayout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -54,6 +54,7 @@ export default function DashboardLayout({
   const navItems = [
     { name: 'Dashboard', path: '/dashboard', icon: 'dashboard' },
     { name: 'All Movies', path: '/dashboard/movies', icon: 'movie' },
+    {  name: 'All Series', path: '/dashboard/series', icon: 'allseries' },
     { name: 'Add Movie', path: '/dashboard/add-movie', icon: 'add' },
     { name:  'Add Series', path: '/dashboard/add-series', icon: 'Series' },
     { name: 'Bulk Upload', path: '/dashboard/bulk-upload', icon: 'upload' },
@@ -219,6 +220,12 @@ function IconForNav({ name }: { name: string }) {
       return (
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
           <path d="M0 1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V1zm4 0v6h8V1H4zm8 8H4v6h8V9zM1 1v2h2V1H1zm2 3H1v2h2V4zM1 7v2h2V7H1zm2 3H1v2h2v-2zm-2 3v2h2v-2H1zM15 1h-2v2h2V1zm-2 3v2h2V4h-2zm2 3h-2v2h2V7zm-2 3v2h2v-2h-2zm2 3h-2v2h2v-2z"/>
+        </svg>
+      );
+    case 'allseries':
+      return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M4 1a1 1 0 0 1 1 1v12a1 1 0 0 1-2 0V2a1 1 0 0 1 1-1zm8 0a1 1 0 0 1 1 1v12a1 1 0 0 1-2 0V2a1 1 0 0 1 1-1z"/>
         </svg>
       );
     case 'add':
